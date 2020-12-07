19b621f8bb463242660160449dd8ded262e8f9be
Goop0 <affe.gardebro@elev.ga.ntig.se>
2020-12-07 14:34:23 +0100

    extra

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Renop project
  text baserat spel
+inte helt säker på hur det kommer bli, men jag tänker mig en character som heter Rob som ska stjäla lite
---
+++ Text.py
@@ -0,0 +1,23 @@
+image r icon1 = 'Rob.png'
+
+
+
+define r = Character ('Rob', color='#9161CC')
+
+# The game starts here.
+
+label start:
+
+
+
+    scene
+
+
+
+    show r icon1
+
+
+
+    r "You've created a new Ren'Py game."
+
+    r "Once you add a story, pictures, and music, you can release it to the world!"
